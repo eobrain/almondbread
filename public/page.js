3ca6c41dbe9abe946f69723c11fc094d5ded361c
Eamonn O'Brien-Strain <eob@well.com>
2020-12-29 22:47:05 -0800

    Respond to click.

--- public/page.js
+++ public/page.js
@@ -1,5 +1,8 @@
 /* global imgElement, xElement, yElement, wElement, iElement */
 
+const imgWidth = 1400
+const imgHeight = 900
+
 const debounce = (() => {
   let suppressed = false
   const suppress = () => {
@@ -23,6 +26,20 @@ const doit = () => {
   const i = iElement.value
   console.log('values:', x, y, w, i)
   imgElement.setAttribute('src', `/image?x=${x}&y=${y}&w=${w}&i=${i}`)
+  imgElement.onclick = event => {
+    const xCoordinate = event.offsetX
+    const yCoordinate = event.offsetY
+    const scale = w / imgWidth
+    const height = scale * imgHeight
+    const viewPortLeft = x - w / 2
+    const viewPortTop = y - height / 2
+    const newX = viewPortLeft + xCoordinate * scale
+    const newY = viewPortTop + yCoordinate * scale
+    console.log({ xCoordinate, yCoordinate, newX, newY })
+    xElement.value = newX
+    yElement.value = newY
+    doit()
+  }
 }
 
 window.onload = () => {
