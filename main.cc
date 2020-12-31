0e3a62c4fef41531eb80e7e836ff1f61a2ed2334
Eamonn O'Brien-Strain <eob@well.com>
2020-12-30 23:07:05 -0800

    Nicer colors.

--- main.cc
+++ main.cc
@@ -26,6 +26,11 @@ using std::string;
 using std::thread;
 using std::vector;
 
+//#define P(x) cout << __FILE__ << ":" << __LINE__ << ":" << #x << "=" << x <<
+// endl
+
+#define P(x)
+
 namespace {
 
 constexpr int INT_MIN = std::numeric_limits<int>::min();
@@ -44,30 +49,33 @@ ostream &operator<<(ostream &out, const Params &p) {
   out << "imgWidth=" << p.imgWidth << " imgHeight=" << p.imgHeight
       << " centerRe=" << p.centerRe << " centerIm=" << p.centerIm
       << " width=" << p.width << " maxIterationCount=" << p.maxIterationCount
-      << " outputFileName" << p.outputFileName;
+      << " outputFileName=" << p.outputFileName;
   return out;
 }
 
 class Stats {
-  map<int, int> histogram;
-  int min = INT_MAX;
-  int max = INT_MIN;
+  map<int, int> _histogram;
+  int _min = INT_MAX;
+  int _max = INT_MIN;
 
  public:
   void operator()(int i) {
-    if (i > max) max = i;
-    if (i < min) min = i;
-    auto lookup = histogram.find(i);
-    int prev = lookup == histogram.end() ? 0 : lookup->second;
-    histogram[i] = prev + 1;
+    P(i);
+    if (i > _max) _max = i;
+    if (i < _min) _min = i;
+    auto lookup = _histogram.find(i);
+    int prev = lookup == _histogram.end() ? 0 : lookup->second;
+    _histogram[i] = prev + 1;
   }
+  int mapped(int iterations) const { return iterations - _min; }
+  int range() const { return _max - _min; }
   friend ostream &operator<<(ostream &, const Stats &);
 };
 ostream &operator<<(ostream &out, const Stats &s) {
   out << "Iterations,Count\n";
-  for (int i = s.min; i <= s.max; ++i) {
-    auto lookup = s.histogram.find(i);
-    if (lookup != s.histogram.end()) {
+  for (int i = s._min; i <= s._max; ++i) {
+    auto lookup = s._histogram.find(i);
+    if (lookup != s._histogram.end()) {
       out << i << "," << lookup->second << "\n";
     }
   }
@@ -102,7 +110,7 @@ class Image {
     unsigned error = lodepng::encode(params.outputFileName, _pixels,
                                      params.imgWidth, params.imgHeight);
     if (error) {
-      cout << "encoder error " << error << ": " << lodepng_error_text(error)
+      cerr << "encoder error " << error << ": " << lodepng_error_text(error)
            << endl;
       return false;
     }
@@ -111,10 +119,9 @@ class Image {
 };
 
 array<double, 3> hsv2rgb(double h, double s, double v) {
-  double c = 0.0, m = 0.0, x = 0.0;
-  c = v * s;
-  x = c * (1.0 - fabs(fmod(h / 60.0, 2) - 1.0));
-  m = v - c;
+  double c = v * s;
+  double x = c * (1.0 - fabs(fmod(h / 60.0, 2) - 1.0));
+  double m = v - c;
   if (h >= 0.0 && h < 60.0) {
     return {c + m, x + m, m};
   }
@@ -174,7 +181,8 @@ void setColor(Image *img, int maxIterationCount, int ix, int iy) {
   }
 
   double value = log(iters) / log(maxIterationCount);
-  auto rgb = hsv2rgb(250*value, 0.5, value);
+  // double value = log(stats.mapped(iters)) / log(stats.range());
+  auto rgb = hsv2rgb(250 * value, 1 - value, 1 - value);
   for (int i = 0; i < 3; ++i) {
     img->pixel(ix, iy, i) = rgb[i] * 256;
   }
@@ -194,7 +202,7 @@ void threadWorker(const Params &params, Image *img, int mod) {
 }  // namespace
 
 int main(int argc, char *const argv[]) {
-  cout << "threadCount=" << threadCount << endl;
+  P(threadCount);
   Params params;
 
   int opt;
@@ -233,7 +241,7 @@ int main(int argc, char *const argv[]) {
     }
   }
 
-  cout << params << endl;
+  P(params);
 
   Image img(params.imgWidth, params.imgHeight);
 
@@ -250,13 +258,7 @@ int main(int argc, char *const argv[]) {
       setColor(&img, params.maxIterationCount, ix, iy);
     }
 
-  cout << "maxIterationCount=" << params.maxIterationCount
-       << " sqrt=" << sqrt(params.maxIterationCount)
-       << " log=" << log(params.maxIterationCount) << endl
-       << int(COLOR_SCALE * params.maxIterationCount) << " "
-       << int(COLOR_SCALE * sqrt(params.maxIterationCount)) << " "
-       << int(COLOR_SCALE * log(params.maxIterationCount)) << endl
-       << "centerIterations=" << img.centerIterations() << endl;
+  P(params);
   bool ok = img.writePng(params);
 
   ofstream histogram("histogram.csv");
