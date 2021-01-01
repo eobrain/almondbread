1f48c2ff81a8c565f9200af001546d2f4d85e7c4
Eamonn O'Brien-Strain <eob@well.com>
2021-01-01 11:00:59 -0800

    Control magnification

--- public/client.js
+++ public/client.js
@@ -1,37 +1,24 @@
-/* global imgElement, xElement, yElement, wElement, iElement, consoleElement */
+/* global imgElement, magnificationElement, xElement, yElement, wElement, iElement, consoleElement */
 
 import { imgWidth, imgHeight } from './common.js'
-
-/* const debounce = (() => {
-  let suppressed = false
-  const suppress = () => {
-    if (suppressed) {
-      return true
-    }
-    suppressed = true
-    setTimeout(() => { suppressed = false }, 1000)
-    return false
-  }
-  return { suppress }
-})() */
 
 let busy = false
 
 const doit = () => {
-  // if (debounce.suppress()) {
-  //  return
-  // }
   if (!window.location.hash) {
     window.location = '#0_0_8_100'
   }
   const [x, y, w, i] = window.location.hash.substr(1).split('_')
+  xElement.innerText = x
+  yElement.innerText = y
+  wElement.innerText = w
   iElement.value = i
   console.log('values:', x, y, w, i)
   busy = true
   consoleElement.innerText = 'Busy ...'
   imgElement.setAttribute('src', `/image?x=${x}&y=${y}&w=${w}&i=${i}`)
   imgElement.onload = () => {
-    consoleElement.innerText = 'Got image'
+    consoleElement.innerText = ''
     busy = false
   }
   imgElement.onclick = event => {
@@ -48,10 +35,8 @@ const doit = () => {
     const newX = viewPortLeft + xCoordinate * scale
     const newY = viewPortTop + yCoordinate * scale
     console.log({ xCoordinate, yCoordinate, newX, newY })
-    xElement.value = newX
-    yElement.value = newY
-    wElement.value = w / 10
-    window.location = `#${newX}_${newY}_${w / 10}_${iElement.value}`
+    const magnification = magnificationElement.value
+    window.location = `#${newX}_${newY}_${w / (1 << magnification)}_${iElement.value}`
     doit()
   }
 }
